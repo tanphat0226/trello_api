5a078a76209e04a1b509d4d410eeb614080f5c22
ryanpham <ryanphamit@gmail.com>
2023-12-12 21:37:26 +0700

    Update Whitelist Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-phamtanphat0226.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
