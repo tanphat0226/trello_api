ce22f012356907605fc83968bf1acddbcaa0740a
ryanpham <ryanphamit@gmail.com>
2023-12-12 20:58:15 +0700

    Update CORS not allow localhost

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,12 +6,12 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế (Video số 62 trong chuỗi MERN Stack Pro)
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // If the environment is local dev then let it run
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
 
+    // If the environment is production
     // Kiểm tra xem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
       return callback(null, true)
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'http://localhost:5173'
+  // 'http://localhost:5173'
 ]
 
 export const BOARD_TYPES = {
