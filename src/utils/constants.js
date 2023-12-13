397779f0970e7c0ffa15c8741fa3d18119efad8c
ryanpham <ryanphamit@gmail.com>
2023-12-13 20:57:27 +0700

    Fix WL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,9 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
-  'https://trello-web-kappa.vercel.app/',
-  'https://trello-web-phamtanphat0226.vercel.app/',
-  'https://trello-ezc3z7rm0-phamtanphat0226.vercel.app/'
+  'https://trello-web-kappa.vercel.app',
+  'https://trello-web-phamtanphat0226.vercel.app',
+  'https://trello-ezc3z7rm0-phamtanphat0226.vercel.app'
 ]
 
 export const BOARD_TYPES = {
